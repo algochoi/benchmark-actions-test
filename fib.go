9220981986939b60d24a98cc8ed75f1f54f65403
algochoi <86622919+algochoi@users.noreply.github.com>
2022-05-13 17:35:12 -0400

    Again...

--- fib.go
+++ fib.go
@@ -6,10 +6,10 @@ func Fib(u uint) uint {
 	if u <= 1 {
 		return 1
 	}
-	for i := 1; i <= 1; i++ {
+	for i := 1; i <= 10; i++ {
 		x := 1
 		x++
-		time.Sleep(5)
+		time.Sleep(1)
 	}
 
 	return Fib(u-2) + Fib(u-1)
