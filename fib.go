db2de0f07695fb970add8eb08c2cb8badd81e383
algochoi <86622919+algochoi@users.noreply.github.com>
2022-05-13 17:14:58 -0400

    Add delay in benchmarks

--- fib.go
+++ fib.go
@@ -1,12 +1,15 @@
 package fib
 
+import "time"
+
 func Fib(u uint) uint {
 	if u <= 1 {
 		return 1
 	}
-	for i := 1; i <= 1000; i++ {
+	for i := 1; i <= 10; i++ {
 		x := 1
 		x++
+		time.Sleep(1)
 	}
 
 	return Fib(u-2) + Fib(u-1)
